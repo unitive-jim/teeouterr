0fb205cddb534ab582e84350a569fb2d17324d87
Jim Lloyd <jim@unitive.works>
2017-05-10 15:50:37 -0700

    Remove listeners to fix emitter leak warning

--- runner.js
+++ runner.js
@@ -33,7 +33,7 @@ function run({executable, args, output}, done) {
 
   // Create a promise that will be resolved when the child process has exited.
   const exited = new P(resolve => {
-    child.on('exit', (code, signal) => {
+    child.once('exit', (code, signal) => {
       const exitCode = asExitCode(code, signal);
       process.exitCode = exitCode;
       resolve(exitCode);
@@ -44,14 +44,14 @@ function run({executable, args, output}, done) {
   const closed = new P(resolve => child.on('close', (code, signal) => resolve(asExitCode(code, signal))));
 
   // When the child closes its stdin stream, close the parent process stdin
-  child.stdin.on('close', () => process.stdin.end());
+  child.stdin.once('close', () => process.stdin.end());
 
   // When any data is output by the child, call the `output` function with the data.
   child.stdout.on('data', output);
   child.stderr.on('data', output);
 
   // When the parent process stdin sees end of stream, tell the child that it will not receive any more input
-  process.stdin.on('end', () => child.stdin.end());
+  process.stdin.once('end', () => child.stdin.end());
 
   // When the parent process receives any data, forward it to the child process
   process.stdin.on('readable', () => {
@@ -75,6 +75,8 @@ function run({executable, args, output}, done) {
     if (exitCode === UNRECOGNIZED_SIGNAL) {
       process.stderr.write(`Child process terminated with unrecognized signal\n`);
     }
+    const emitters = [child, child.stdin, child.stdout, child.stderr, process.stdin, process.stdout, process.stderr];
+    emitters.forEach(e => e.removeAllListeners());
     return exitCode;
   })
   .asCallback(done);
