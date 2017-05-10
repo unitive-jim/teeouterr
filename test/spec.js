e13716e5390f1b1028d7d8ae8bd86b248cb45f4a
Jim Lloyd <jim@unitive.works>
2017-05-10 15:41:13 -0700

    Refactor, and add CLI tests

--- test/spec.js
+++ test/spec.js
@@ -1,8 +1,69 @@
 const chai = require('chai');
 const crypto = require('crypto');
+const fs = require('fs');
 const runner = require('../runner');
 
 const expect = chai.expect;
+
+function tempFilePath() {
+  const name = crypto.randomBytes(10).toString('hex');
+  return `/tmp/${name}.tmp`;
+}
+
+function testWithBlaster(parent, numLines) {
+  let tempFile;
+  let cmdline;
+  if (parent) {
+    tempFile = tempFilePath();
+    cmdline = `${parent} ${tempFile} ./testers/blaster.js ${numLines}`;
+  } else {
+    cmdline = `./testers/blaster.js ${numLines}`;
+  }
+
+  it(cmdline, function () {
+    if (numLines >= 20000) {
+      this.timeout(100 * numLines); // A rough heuristic that works on a MacBook with plenty of headroom
+    }
+    let chunks = [];
+    function output(data) {
+      chunks.push(data.toString());
+    }
+
+    const [executable, ...args] = cmdline.split(' ');
+    return runner.run({executable, args, output})
+    .then(() => {
+      const lines = chunks.join('').split('\n');
+      validateChecksum(lines, numLines);
+    })
+    .then(() => {
+      if (tempFile) {
+        // The temp file should also have contents that pass validateChecksum()
+        const lines = fs.readFileSync(tempFile, {encoding: 'utf8'}).split('\n');
+        expect(lines).to.have.length.above(2);
+        validateChecksum(lines, numLines);
+        fs.unlinkSync(tempFile);
+      }
+    });
+  });
+}
+
+function validateChecksum(lines, numLines) {
+  if (lines.length <= 1) {
+    return;
+  }
+  expect(lines).to.have.lengthOf(numLines+2);
+  expect(lines[0]).to.equal(`${numLines}`);
+
+  const [expectedCheckSum] = lines.slice(-1);
+  expect(expectedCheckSum).to.have.lengthOf(64);
+
+  const bigBuf = lines.slice(0, -1).join('\n') + '\n';
+  const hash = crypto.createHash('sha256');
+  hash.update(bigBuf);
+  const actualChecksum = hash.digest('hex');
+
+  expect(actualChecksum).to.equal(expectedCheckSum);
+};
 
 describe('runner', function() {
 
@@ -29,42 +90,35 @@ describe('runner', function() {
     });
   });
 
-  function testWithBlaster(numLines) {
-    it(`blaster ${numLines}`, function () {
-      if (numLines >= 20000) {
-        this.timeout(100 * numLines); // A rough heuristic that works on a MacBook with plenty of headroom
-      }
-      let chunks = [];
-      function output(data) {
-        chunks.push(data.toString());
-      }
-      const numLinesAsString = `${numLines}`;
-      const executable = './testers/blaster.js';
-      const args = [numLinesAsString];
-      return runner.run({executable, args, output})
-      .then(() => {
-        const lines = chunks.join('').split('\n');
-        expect(lines).to.have.lengthOf(numLines+2);
-        expect(lines[0]).to.equal(numLinesAsString);
+  describe('stress test', function() {
+    const parent = null;
+    testWithBlaster(parent, 2);
+    testWithBlaster(parent, 20);
+    testWithBlaster(parent, 20000);
+    testWithBlaster(parent, 200000);
+  });
+});
 
-        const [expectedCheckSum] = lines.slice(-1);
-        expect(expectedCheckSum).to.have.lengthOf(64);
-
-        const bigBuf = lines.slice(0, -1).join('\n') + '\n';
-        const hash = crypto.createHash('sha256');
-        hash.update(bigBuf);
-        const actualChecksum = hash.digest('hex');
-
-        expect(actualChecksum).to.equal(expectedCheckSum);
-      });
-    });
-  }
+describe('teeouterr', function() {
 
   describe('stress test', function() {
-    testWithBlaster(2);
-    testWithBlaster(20);
-    testWithBlaster(20000);
-    testWithBlaster(200000);
-    // testWithBlaster(2000000);   // this passes, but is slow and probably overkill
+    const parent = './teeouterr.js';
+    testWithBlaster(parent, 2);
+    testWithBlaster(parent, 20);
+    testWithBlaster(parent, 20000);
+    testWithBlaster(parent, 200000);
   });
+
+});
+
+describe('mergeouterr', function() {
+
+  describe('stress test', function() {
+    const parent = './mergeouterr.js';
+    testWithBlaster(parent, 2);
+    testWithBlaster(parent, 20);
+    testWithBlaster(parent, 20000);
+    testWithBlaster(parent, 200000);
+  });
+
 });
